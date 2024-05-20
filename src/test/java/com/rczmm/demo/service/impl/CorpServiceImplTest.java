d449c246ebf8c00f1db501985227922dc7ca19db
rczmm <3179623450@qq.com>
2024-05-20 22:53:25 +0800

    单元测试

---
+++ src/test/java/com/rczmm/demo/service/impl/CorpServiceImplTest.java
@@ -0,0 +1,112 @@
+package com.rczmm.demo.service.impl;
+
+import com.rczmm.demo.domain.Corp;
+import com.rczmm.demo.mapper.CorpMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CorpServiceImplTest {
+
+    @Mock
+    private CorpMapper corpMapper;
+
+
+    @InjectMocks
+    private CorpServiceImpl corpService;
+
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("开始测试");
+        corpService = new CorpServiceImpl(corpMapper);
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("测试结束");
+        corpService = null;
+    }
+
+    @Transactional(readOnly = true)
+    @Test
+    void selectCorpById() {
+        long id = 1;
+        corpMapper.selectCorpById(id);
+        verify(corpMapper).selectCorpById(id);
+    }
+
+    @Transactional(readOnly = true)
+    @Test
+    void selectCorpList() {
+        Corp corp = new Corp();
+        corpMapper.selectCorpList(corp);
+        verify(corpMapper).selectCorpList(corp);
+    }
+
+    @Rollback
+    @Test
+    void insertCorp() {
+        Corp corp = new Corp();
+        corp.setPid(2L);
+        corp.setType("诈骗部");
+        corp.setStatus("1");
+        corp.setName("诈骗司");
+        corp.setCreateTime("2020-01-01 12:00:00");
+        corp.setUpdateTime("2021-01-01 12:00:00");
+        corp.setDescription("诈骗公司");
+        when(corpMapper.insertCorp(corp)).thenReturn(1);
+        int result = corpMapper.insertCorp(corp);
+        verify(corpMapper).insertCorp(corp);
+        Assertions.assertEquals(1, result);
+    }
+
+    @Rollback
+    @Test
+    void updateCorp() {
+        Corp corp = new Corp();
+        corp.setId(1L);
+        corp.setPid(2L);
+        corp.setType("诈骗部");
+        corp.setStatus("1");
+        corp.setName("诈骗司");
+        corp.setCreateTime("2020-01-01 12:00:00");
+        corp.setUpdateTime("2021-01-01 12:00:00");
+        corp.setDescription("诈骗公司");
+        when(corpMapper.updateCorp(corp)).thenReturn(1);
+        int result = corpMapper.updateCorp(corp);
+        verify(corpMapper).updateCorp(corp);
+        Assertions.assertEquals(1, result);
+    }
+
+    @Rollback
+    @Test
+    void deleteCorpByIds() {
+        Long[] ids = {1L, 2L};
+        when(corpMapper.deleteCorpByIds(ids)).thenReturn(1);
+        int result = corpMapper.deleteCorpByIds(ids);
+        verify(corpMapper).deleteCorpByIds(ids);
+        Assertions.assertEquals(1, result);
+    }
+
+    @Rollback
+    @Test
+    void deleteCorpById() {
+        long id = 1;
+        when(corpMapper.deleteCorpById(id)).thenReturn(1);
+        int result = corpMapper.deleteCorpById(id);
+        verify(corpMapper).deleteCorpById(id);
+        Assertions.assertEquals(1, result);
+    }
+}
---
+++ src/test/java/com/rczmm/demo/service/impl/UserServiceImplTest.java
@@ -0,0 +1,107 @@
+package com.rczmm.demo.service.impl;
+
+import com.rczmm.demo.domain.User;
+import com.rczmm.demo.mapper.UserMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+
+    @Mock
+    private UserMapper userMapper;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("开始测试");
+        userService = new UserServiceImpl(userMapper);
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("测试结束");
+        userService = null;
+    }
+
+    @Transactional(readOnly = true)
+    @Test
+    void selectUserById() {
+        long id = 1;
+        userService.selectUserById(id);
+        verify(userMapper).selectUserById(id);
+    }
+
+    @Transactional(readOnly = true)
+    @Test
+    void selectUserList() {
+        User user = new User();
+        userService.selectUserList(user);
+        verify(userMapper).selectUserList(user);
+    }
+
+    @Rollback
+    @Test
+    void insertUser() {
+        User user = new User();
+        user.setId(1L);
+        user.setCorpId(1L);
+        user.setName("data");
+        user.setAccount("data");
+        user.setCreateTime("2020-01-01 00:00:00");
+        user.setUpdateTime("2020-01-01 00:00:00");
+        when(userMapper.insertUser(user)).thenReturn(1);
+        int result = userService.insertUser(user);
+        verify(userMapper).insertUser(user);
+        Assertions.assertEquals(1, result);
+    }
+
+    @Rollback
+    @Test
+    void updateUser() {
+        User user = new User();
+        user.setId(1L);
+        user.setCorpId(1L);
+        user.setName("data");
+        user.setAccount("data");
+        user.setCreateTime("2020-01-01 00:00:00");
+        user.setUpdateTime("2020-01-01 00:00:00");
+        when(userMapper.updateUser(user)).thenReturn(1);
+        int result = userService.updateUser(user);
+        verify(userMapper).updateUser(user);
+        Assertions.assertEquals(1, result);
+    }
+
+    @Rollback
+    @Test
+    void deleteUserByIds() {
+        Long[] ids = {1L, 2L, 3L};
+        when(userMapper.deleteUserByIds(ids)).thenReturn(1);
+        int result = userService.deleteUserByIds(ids);
+        verify(userMapper).deleteUserByIds(ids);
+        Assertions.assertEquals(1, result);
+    }
+
+    @Rollback
+    @Test
+    void deleteUserById() {
+        long id = 1;
+        when(userMapper.deleteUserById(id)).thenReturn(1);
+        int result = userService.deleteUserById(id);
+        verify(userMapper).deleteUserById(id);
+        Assertions.assertEquals(1, result);
+    }
+}
